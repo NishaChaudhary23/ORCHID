b03e7cc2e41610e01fdd29ddef0c106597b252bb
Nisha Chaudhary <37832034+NishaChaudhary23@users.noreply.github.com>
2024-05-04 18:34:46 +0530

    Add files via upload

---
+++ train-normal-osmf-oscc.sh
@@ -0,0 +1,1 @@
+python3 ./tools/train-classify-normal-osmf-oscc.py /media/microcrispr8/DATA\ 1/ORCHID-April24/model-1 InceptionV3
---
+++ train-wdoscc-mdoscc-pdoscc.sh
@@ -0,0 +1,1 @@
+python3 ./tools/train-classify-wdoscc-mdoscc-pdoscc.py /media/microcrispr8/DATA\ 1/ORCHID-April24/model-2 InceptionV3
